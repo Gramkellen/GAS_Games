dc7cde9d592d4332bf160dff9151021badf31405
Kellen Gram <kellen2051877@gmail.com>
2024-09-16 21:32:57 +0800

    Make BP

--- Source/Aura/Private/Character/AuroCharacterBase.cpp
+++ Source/Aura/Private/Character/AuroCharacterBase.cpp
@@ -7,7 +7,8 @@ AAuroCharacterBase::AAuroCharacterBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
-
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(),FName("WeaponHandleSocket"));
 }
 
 void AAuroCharacterBase::BeginPlay()
