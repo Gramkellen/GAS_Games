80acc9a657d101bd83e281012bab11c0baeeb89a
Kellen Gram <kellen2051877@gmail.com>
2024-09-21 17:41:04 +0800

    Add Player Controller And Movement

--- Source/Aura/Private/Player/AuroPlayerController.cpp
+++ Source/Aura/Private/Player/AuroPlayerController.cpp
@@ -2,4 +2,54 @@
 
 
 #include "Player/AuroPlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+AAuroPlayerController::AAuroPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuroPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(Context);
+	// 获取本地玩家的一个输入系统
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if(Subsystem!=nullptr)
+	{
+		Subsystem->AddMappingContext(Context,0);
+		bShowMouseCursor = true;
+		DefaultMouseCursor = EMouseCursor::Default;
+
+		FInputModeGameAndUI InputMode;
+		InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+		InputMode.SetHideCursorDuringCapture(false);
+		SetInputMode(InputMode);
+	}
+}
+
+void AAuroPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent *EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&AAuroPlayerController::Move);
+}
+
+void AAuroPlayerController::Move(const FInputActionValue& ActionValue)
+{
+	// FInputAction起始是一个对应上和右的一个结构体
+	const FVector2D InputAxisVector = ActionValue.Get<FVector2D>();
+	FRotator Rotation = GetControlRotation();
+	FRotator YawRotation{0.f,Rotation.Yaw,0.f};
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	
+	if(APawn* PawnController = GetPawn<APawn>())
+	{
+		// 对应了WSAD
+		PawnController->AddMovementInput(ForwardDirection,InputAxisVector.Y);
+		PawnController->AddMovementInput(RightDirection,InputAxisVector.X);
+	}
+}
--- Source/Aura/Public/Player/AuroPlayerController.h
+++ Source/Aura/Public/Player/AuroPlayerController.h
@@ -3,9 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputActionValue.h"
 #include "GameFramework/PlayerController.h"
 #include "AuroPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
 /**
  * 
  */
@@ -13,5 +16,22 @@ UCLASS()
 class AURA_API AAuroPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	AAuroPlayerController();
+
+protected:
+	void BeginPlay() override;
+
+    void SetupInputComponent() override;
+private:
+	// 映射的上下文
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputMappingContext>Context;
+
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputAction>MoveAction;
+
+	void Move(const FInputActionValue& ActionValue);
 	
 };
