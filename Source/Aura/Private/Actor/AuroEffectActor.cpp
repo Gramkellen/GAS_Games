317df7c5eb1123c547d471cac1d15a24050e46c9
Kellen Gram <kellen2051877@gmail.com>
2024-10-14 20:14:10 +0800

    Add Infinite GameEffect

--- Source/Aura/Private/Actor/AuroEffectActor.cpp
+++ Source/Aura/Private/Actor/AuroEffectActor.cpp
@@ -30,8 +30,70 @@ void AAuroEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGam
 	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
 	EffectContextHandle.AddSourceObject(this);
 	// 不直接使用Effect,通过一个Handle来管理
-	FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.0f,EffectContextHandle);
-	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.0f,EffectContextHandle);
+	const FActiveGameplayEffectHandle ActiveEffectSpecHandle = TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	// Def 是一个弱指针，通过弱指针来进行访问
+	bool bInfinite = EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite;
+	if(bInfinite && IsValid(TargetASC))
+	{
+		ActiveHandlesMap.Add(ActiveEffectSpecHandle, TargetASC);
+	}
+}
+
+void AAuroEffectActor::OnOverlap(AActor* TargetActor)
+{
+	if(InstantApplicationPolicy == EEffectApplicationPolicy::EAP_ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if(DurationApplicationPolicy == EEffectApplicationPolicy::EAP_ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if(InfiniteApplicationPolicy == EEffectApplicationPolicy::EAP_ApplyOnOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+}
+
+void AAuroEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if(InstantApplicationPolicy == EEffectApplicationPolicy::EAP_ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+	}
+	if(DurationApplicationPolicy == EEffectApplicationPolicy::EAP_ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+	}
+	if(InfiniteApplicationPolicy == EEffectApplicationPolicy::EAP_ApplyOnEndOverlap)
+	{
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+	}
+
+	// When EndOverlap , Remove Effect
+	if(InfiniteEffectRemovePolicy == EEffectRemovalPolicy::ERP_RemoveOnEndOverlap)
+	{
+		UAbilitySystemComponent *TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+		if(TargetASC == nullptr) return;
+		TArray<FActiveGameplayEffectHandle> HandlestoRemove;
+		
+		for(auto HandlesTuple : ActiveHandlesMap)
+		{
+			if (HandlesTuple.Value == TargetASC)
+			{
+				// 移除 效果
+				TargetASC->RemoveActiveGameplayEffect(HandlesTuple.Key);
+				HandlestoRemove.Add(HandlesTuple.Key);
+				
+			}
+		}
+		// 从 Map 中进行移除
+		for(FActiveGameplayEffectHandle& Handle : HandlestoRemove)
+		{
+			ActiveHandlesMap.FindAndRemoveChecked(Handle);
+		}
+	}
 }
 
 
--- Source/Aura/Public/Actor/AuroEffectActor.h
+++ Source/Aura/Public/Actor/AuroEffectActor.h
@@ -4,9 +4,29 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "GameplayEffectTypes.h"
 #include "AuroEffectActor.generated.h"
 
+class UAbilitySystemComponent;
+struct FActiveGameplayEffectHandle;
 class UGameplayEffect;
+
+// 标记什么时候枚举发挥作用
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy
+{
+	EAP_ApplyOnOverlap,
+	EAP_ApplyOnEndOverlap,
+	EAP_DoNotApply
+};
+
+// 移除 GE 的政策
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy
+{
+	ERP_RemoveOnEndOverlap,
+	ERP_DoNotRemove
+};
 
 UCLASS()
 class AURA_API AAuroEffectActor : public AActor
@@ -21,12 +41,38 @@ protected:
 
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor *TargetActor, TSubclassOf<UGameplayEffect>GameplayEffectClass);
+
+	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
+	bool bDestroyOnEffectRemoval = false;
 	
 	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Effects")
-	TSubclassOf<UGameplayEffect>InstanceGameplayEffectClass;
+	TSubclassOf<UGameplayEffect>InstantGameplayEffectClass;
 
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Effect")
+	EEffectApplicationPolicy InstantApplicationPolicy = EEffectApplicationPolicy::EAP_DoNotApply;
+	
 	UPROPERTY(EditAnywhere,BlueprintReadOnly, Category = "Effects")
 	TSubclassOf<UGameplayEffect>DurationGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Effect")
+	EEffectApplicationPolicy DurationApplicationPolicy = EEffectApplicationPolicy::EAP_DoNotApply;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Effects")
+	TSubclassOf<UGameplayEffect>InfiniteGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Effects")
+	EEffectApplicationPolicy InfiniteApplicationPolicy = EEffectApplicationPolicy::EAP_DoNotApply;
+
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category = "Effects")
+	EEffectRemovalPolicy InfiniteEffectRemovePolicy = EEffectRemovalPolicy::ERP_DoNotRemove;
+
+	TMap<FActiveGameplayEffectHandle, UAbilitySystemComponent*> ActiveHandlesMap;
 };
 
 
